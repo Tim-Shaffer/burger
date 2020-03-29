aac618068ac61d667b3ee5a378f3c9a082f8f9e1
Tim-Shaffer <tcshaf24@ptd.net>
2020-03-29 17:49:45 -0400

    updated comments in the orm.js config file

--- config/orm.js
+++ config/orm.js
@@ -1,18 +1,30 @@
+// =============================================================
+// Used Activity 16 as a starting point
+// =============================================================
+
+// =============================================================
+// Dependencies
+// =============================================================
 var connection = require("../config/connection.js");
 
-//  From Activity 16 - MvcExample
 // Helper function to convert object key/value pairs to SQL syntax
 function objToSql(ob) {
+
   var arr = [];
 
   // loop through the keys and push the key/value as a string int arr
   for (var key in ob) {
+
     var value = ob[key];
+
     // check to skip hidden properties
     if (Object.hasOwnProperty.call(ob, key)) {
+
       // if string with spaces, add quotations 
       if (typeof value === "string" && value.indexOf(" ") >= 0) {
+
         value = "'" + value + "'";
+
       }
       
       // e.g. {devored: true} => ["devoured=true"]
@@ -22,58 +34,85 @@ function objToSql(ob) {
 
   // translate array of strings to a single comma-separated string
   return arr.toString();
+
 };
 
+// declare an orm object variable
 var orm = {
   
+  // create a selectAll method to return all rows from a provided table
   selectAll: function(tableInput, cb) {
-      var queryString = "SELECT * FROM " + tableInput + ";";
+
+    // build the SQL query syntax for the select statement  
+    var queryString = "SELECT * FROM " + tableInput + ";";
+
+      // use the existing db connection and call the query method with the built queryString
       connection.query(queryString, function(err, result) {
+
         if (err) {
+ 
           throw err;
+ 
         }
+
+        // callback function to wait for the query and then return the results
         cb(result);
+
       });
     
   },
 
+  // create an insertOne method to insert a new row into the provided table
   insertOne: function(table, col, val, cb) {
+
+    // build the SQL query syntax for the insert statement 
     var queryString = "INSERT INTO " + table + "(";
-    queryString += col.toString();
-    queryString += ") VALUES ('";
-    queryString += val.toString();
-    queryString += "') ";
+      queryString += col.toString();
+      queryString += ") VALUES ('";
+      queryString += val.toString();
+      queryString += "') ";
 
-    console.log(queryString);
-
+    // use the existing db connection and call the query method with the built queryString
     connection.query(queryString, function(err, result) {
+
       if (err) {
+
         throw err;
       }
 
-      cb(result);
+        // callback function to wait for the query and then return the results
+        cb(result);
+
     });
     
   },
 
+  // create an updateOne method to update a row in the provided table
   updateOne: function(table, objColVals, condition, cb) {
+    
+    // build the SQL query syntax for the update statement 
     var queryString = "UPDATE " + table;
+      queryString += " SET ";
+      queryString += objToSql(objColVals);
+      queryString += " WHERE ";
+      queryString += condition;
 
-    queryString += " SET ";
-    queryString += objToSql(objColVals);
-    queryString += " WHERE ";
-    queryString += condition;
-
-    console.log(queryString);
+    // use the existing db connection and call the query method with the built queryString
     connection.query(queryString, function(err, result) {
+      
       if (err) {
+
         throw err;
       }
 
-      cb(result);
+        // callback function to wait for the query and then return the results
+        cb(result);
+
     });
+
   }
 
 };
 
+// export the orm object to be available for use by other files
 module.exports = orm;
