a346cbcb94443e7a01975340da49f059fe54c371
Tim-Shaffer <tcshaf24@ptd.net>
2020-04-02 19:57:29 -0400

    added environment file processing to connection.js

--- config/connection.js
+++ config/connection.js
@@ -7,14 +7,8 @@
 // =============================================================
 var mysql = require("mysql");
 
-// instantiate a new connection object to the mysql database
-// var connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "education",
-//     password: "password",
-//     database: "burgers_db"
-//   });
+// code to read and set any environment variables with the .env package 
+require("dotenv").config();
 
 // create a connection variable to be able to use the JAWSDB setup with Heroku 
 var connection;
@@ -24,10 +18,10 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "localhost",
+    host: process.env.DB_HOST,
     port: 3306,
-    user: "education",
-    password: "password",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
     database: "burgers_db"
   });
 };
